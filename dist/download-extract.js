5be51c9e48f421c2c4269cae02faed1947d2253b
Alex Olivier <alex@alexolivier.me>
2021-11-10 14:39:51 +0400

    file

--- dist/download-extract.js
+++ dist/download-extract.js
@@ -10,21 +10,27 @@ const download_1 = require("./download");
 const tar_gz_1 = __importDefault(require("tar.gz"));
 const get_paths_1 = require("./get-paths");
 const util_1 = require("util");
-const copyFile = (0, util_1.promisify)(fs_1.default.copyFile);
+const readFile = (0, util_1.promisify)(fs_1.default.readFile);
+const writeFile = (0, util_1.promisify)(fs_1.default.writeFile);
 const rm = (0, util_1.promisify)(fs_1.default.rm);
 const chmod = (0, util_1.promisify)(fs_1.default.chmod);
 async function donwloadAndExtract(url, dest) {
     const bundle = path_1.default.join(dest, "engine.tar.gz");
     await (0, download_1.download)(url, bundle);
     await (0, tar_gz_1.default)().extract(bundle, dest);
+    const extractedBinary = path_1.default.join(dest, "cerbos");
     console.log("delete tar.gz");
     await rm(bundle);
     console.log("make executable");
-    await chmod(path_1.default.join(dest, "cerbos"), "755");
+    await chmod(extractedBinary, "755");
     const executablePath = (0, get_paths_1.getExecutablePath)();
     if (executablePath !== dest) {
-        await copyFile(path_1.default.join(dest, "cerbos"), path_1.default.join(executablePath, "cerbos"));
+        console.log("moving to tmp");
+        const data = await readFile(extractedBinary);
+        await writeFile(path_1.default.join(executablePath, "cerbos"), data);
+        await chmod(path_1.default.join(executablePath, "cerbos"), "755");
     }
+    console.log("binary location", executablePath);
 }
 exports.donwloadAndExtract = donwloadAndExtract;
 //# sourceMappingURL=download-extract.js.map
--- src/download-extract.ts
+++ src/download-extract.ts
@@ -6,7 +6,8 @@ import targz from "tar.gz";
 import { getExecutablePath } from "./get-paths";
 import { promisify } from "util";
 
-const copyFile = promisify(fs.copyFile);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 const rm = promisify(fs.rm);
 const chmod = promisify(fs.chmod);
 
@@ -14,15 +15,18 @@ export async function donwloadAndExtract(url: string, dest: string) {
   const bundle = path.join(dest, "engine.tar.gz");
   await download(url, bundle);
   await targz().extract(bundle, dest);
+  const extractedBinary = path.join(dest, "cerbos");
   console.log("delete tar.gz");
   await rm(bundle);
   console.log("make executable");
-  await chmod(path.join(dest, "cerbos"), "755");
+  await chmod(extractedBinary, "755");
+
   const executablePath = getExecutablePath();
   if (executablePath !== dest) {
-    await copyFile(
-      path.join(dest, "cerbos"),
-      path.join(executablePath, "cerbos")
-    );
+    console.log("moving to tmp");
+    const data = await readFile(extractedBinary);
+    await writeFile(path.join(executablePath, "cerbos"), data);
+    await chmod(path.join(executablePath, "cerbos"), "755");
   }
+  console.log("binary location", executablePath);
 }
