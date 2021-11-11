8ee6225208dd1a304129ba25bfe03d464cb3e5c9
Alex Olivier <alex@alexolivier.me>
2021-11-11 11:32:32 +0400

    paths

--- dist/main.js
+++ dist/main.js
@@ -7,38 +7,19 @@ const path_1 = __importDefault(require("path"));
 const cerbos_1 = require("cerbos");
 const cross_spawn_1 = __importDefault(require("cross-spawn"));
 const http_1 = __importDefault(require("http"));
-const fs_1 = __importDefault(require("fs"));
-const temp_dir_1 = __importDefault(require("temp-dir"));
 const CERBOS_ENDPOINT = "http://localhost:3592";
 async function getLocalClient() {
-    var _a, _b;
-    console.log("__dirname", __dirname);
-    console.log("eval('__dirname')", eval("__dirname"));
-    console.log("process.cwd()", process.cwd());
-    console.log(`=== files: ${temp_dir_1.default}`);
-    fs_1.default.readdirSync(temp_dir_1.default).forEach((file) => {
-        console.log(file);
-    });
-    console.log(`=== files: ${process.cwd()}/node_modules/.cerbos`);
-    fs_1.default.readdirSync(`${process.cwd()}/node_modules/.cerbos`).forEach((file) => {
-        console.log(file);
-    });
-    console.log("===");
-    console.log(new Date(), "spwaning:", [
-        `${process.cwd()}/node_modules/.cerbos/cerbos`,
+    console.log("spwaning:", [
+        path_1.default.join(__dirname, "../../.cerbos/cerbos"),
         "server",
         "--config",
-        `${process.cwd()}/node_modules/.cerbos/config.yaml`,
+        path_1.default.join(__dirname, "../../.cerbos/config.yaml"),
     ].join(" "));
-    const cmd = (0, cross_spawn_1.default)(`${process.cwd()}/node_modules/.cerbos/cerbos`, ["server", "--config", `${process.cwd()}/node_modules/.cerbos/config.yaml`], {});
-    (_a = cmd.stdout) === null || _a === void 0 ? void 0 : _a.on("data", (data) => {
-        console.log(new Date(), `stdout: ${data}`);
-    });
-    (_b = cmd.stderr) === null || _b === void 0 ? void 0 : _b.on("data", (data) => {
-        console.error(new Date(), `stderr: ${data}`);
+    const cmd = (0, cross_spawn_1.default)(path_1.default.join(__dirname, "../../.cerbos/cerbos"), ["server", "--config", path_1.default.join(__dirname, "../../.cerbos/config.yaml")], {
+        stdio: "inherit",
     });
     cmd.on("close", (code) => {
-        console.log(new Date(), `child process exited with code ${code}`);
+        console.log(`child process exited with code ${code}`);
     });
     await livenessCheck(`${CERBOS_ENDPOINT}/_cerbos/health`);
     return new cerbos_1.Cerbos({
@@ -50,19 +31,19 @@ async function livenessCheck(host) {
         http_1.default
             .get(host)
             .on("error", () => {
-            // console.log("liveness check failed");
+            console.log("liveness check failed");
             setTimeout(() => {
                 return livenessCheck(host).then(resolve, reject);
             }, 100);
         })
             .on("response", () => {
-            console.log(new Date(), "liveness check passed");
+            console.log("liveness check passed");
             resolve();
         });
     });
 }
-path_1.default.join(process.cwd(), "/policies");
-path_1.default.join(process.cwd(), "/node_modules/.cerbos/cerbos");
-path_1.default.join(process.cwd(), "/node_modules/.cerbos/config.yaml");
+path_1.default.join(__dirname, "../../../policies");
+path_1.default.join(__dirname, "../../.cerbos/cerbos");
+path_1.default.join(__dirname, "../../.cerbos/config.yaml");
 exports.default = getLocalClient;
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -2,56 +2,30 @@ import path from "path";
 import { Cerbos } from "cerbos";
 import spawn from "cross-spawn";
 import http from "http";
-import fs from "fs";
-
-import tempDirectory from "temp-dir";
 
 const CERBOS_ENDPOINT = "http://localhost:3592";
 
 async function getLocalClient(): Promise<Cerbos> {
-  console.log("__dirname", __dirname);
-  console.log("eval('__dirname')", eval("__dirname"));
-  console.log("process.cwd()", process.cwd());
-
-  console.log(`=== files: ${tempDirectory}`);
-  fs.readdirSync(tempDirectory).forEach((file) => {
-    console.log(file);
-  });
-
-  console.log(`=== files: ${process.cwd()}/node_modules/.cerbos`);
-  fs.readdirSync(`${process.cwd()}/node_modules/.cerbos`).forEach((file) => {
-    console.log(file);
-  });
-
-  console.log("===");
-
   console.log(
-    new Date(),
     "spwaning:",
     [
-      `${process.cwd()}/node_modules/.cerbos/cerbos`,
+      path.join(__dirname, "../../.cerbos/cerbos"),
       "server",
       "--config",
-      `${process.cwd()}/node_modules/.cerbos/config.yaml`,
+      path.join(__dirname, "../../.cerbos/config.yaml"),
     ].join(" ")
   );
 
   const cmd = spawn(
-    `${process.cwd()}/node_modules/.cerbos/cerbos`,
-    ["server", "--config", `${process.cwd()}/node_modules/.cerbos/config.yaml`],
-    {}
+    path.join(__dirname, "../../.cerbos/cerbos"),
+    ["server", "--config", path.join(__dirname, "../../.cerbos/config.yaml")],
+    {
+      stdio: "inherit",
+    }
   );
 
-  cmd.stdout?.on("data", (data) => {
-    console.log(new Date(), `stdout: ${data}`);
-  });
-
-  cmd.stderr?.on("data", (data) => {
-    console.error(new Date(), `stderr: ${data}`);
-  });
-
   cmd.on("close", (code) => {
-    console.log(new Date(), `child process exited with code ${code}`);
+    console.log(`child process exited with code ${code}`);
   });
 
   await livenessCheck(`${CERBOS_ENDPOINT}/_cerbos/health`);
@@ -66,20 +40,20 @@ async function livenessCheck(host: string): Promise<void> {
     http
       .get(host)
       .on("error", () => {
-        // console.log("liveness check failed");
+        console.log("liveness check failed");
         setTimeout(() => {
           return livenessCheck(host).then(resolve, reject);
         }, 100);
       })
       .on("response", () => {
-        console.log(new Date(), "liveness check passed");
+        console.log("liveness check passed");
         resolve();
       });
   });
 }
 
-path.join(process.cwd(), "/policies");
-path.join(process.cwd(), "/node_modules/.cerbos/cerbos");
-path.join(process.cwd(), "/node_modules/.cerbos/config.yaml");
+path.join(__dirname, "../../../policies");
+path.join(__dirname, "../../.cerbos/cerbos");
+path.join(__dirname, "../../.cerbos/config.yaml");
 
 export default getLocalClient;
